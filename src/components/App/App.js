4c7c0d074a1e330b04d96be79c546f4560263a8e
Andrii Pysmennyi <andrey.ad511989@gmail.com>
2020-12-17 16:13:06 +0200

    add to localStor

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -10,10 +10,10 @@ const shortid = require('shortid');
 class App extends Component {
   state = {
     contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
   };
@@ -63,7 +63,28 @@ class App extends Component {
       contacts: prevState.contacts.filter(contact => contact.id !== contactId),
     }));
   };
+  //////////////////////////////////////////////////
+  componentDidMount() {
+    console.log('App componentDidMount ');
 
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+  /////////////////////////////////////////////////////
+  componentDidUpdate(prevProps, prevState) {
+    console.log('App componentDidUpdate ');
+
+    if (this.state.contacts !== prevState.contacts) {
+      console.log('обновилось поле contacts, записываю contacts в хранилище');
+
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+  /////////////////////////////////////////////////
   render() {
     const visibleContacts = this.visibleContacts();
 
